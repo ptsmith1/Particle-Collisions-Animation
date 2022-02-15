0f004df3493f4a8db2b65fa565e0886c58e70ab4
Philip Smith <69675193+ptsmith1@users.noreply.github.com>
2022-02-15 18:31:18 +0000

    First working version, simulation calculates collisions based on the angle at which particles collide and their velocities. Running the program should load a flask api which shows the animation. Entering data into the input boxes will create a new simulation and animation with the users input, which is then shown.

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-from create_animation import Simulation
+from flask import Flask,request,render_template
+from create_animation import Simulation, Plotting
+import os
+import uuid
 
-from flask import *
-import json, time
-app = Flask(__name__)
+app = Flask(__name__, instance_path="/{project_folder_abs_path}/instance")
 
-@app.route('/', methods=['GET'])
-def home_page():
-    data_set = {'Page': 'Home', 'Message': 'Successfully loaded the Home page', 'Timestamp': time.time()}
-    json_dump = json.dumps(data_set)
-    return json_dump
+@app.route('/', methods=['GET','POST'])
+def index():
+    file_name = "/static/images/animation.gif"
+    request_type_str = request.method
+    sim_input = {"total_time":1e-11, "temperature":300, "particle_count":30}
+    if request_type_str != 'GET':
+        sim_input["total_time"] = request.form.get("run_time", type=float)
+        sim_input["temperature"] = request.form.get("temperature", type=int)
+        sim_input["particle_count"] = request.form.get("particle_count", type=int)
+        file_name = "/static/images/" + uuid.uuid4().hex + ".gif"
+        create_gif(os.getcwd()+file_name, sim_input)
+    else:
+        if os.path.exists(os.getcwd()+file_name) == False:
+            """If the animation does not exist, then the simulation is setup and run and then animated"""
+            create_gif(os.getcwd()+file_name, sim_input)
+    return render_template('index.html', href=file_name)
 
-@app.route('/user/', methods=['GET'])
-def request_page():
-    user_query = str(request.args.get('user')) #/user/?user=USER_NAME
-    data_set = {'Page': 'Home', 'Message': f'Successfully got request for {user_query}', 'Timestamp': time.time()}
-    json_dump = json.dumps(data_set)
-    return json_dump
+def create_gif(file_name, sim_input):
+    sim = Simulation(sim_input)
+    sim.initialise_arrays()
+    sim.run_simulation()
+    plot = Plotting(sim, file_name)
+    plot.start_animation(sim)
 
 if __name__ == '__main__':
     app.run(port=7777)
